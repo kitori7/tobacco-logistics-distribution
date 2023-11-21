1cefa042b5e0b320ec80a556965f5adb65ccb32e
kitori <kitori@126.com>
2023-11-21 20:09:50 +0800

    🐞 fix: 修复打包和优化

--- src/op.ts
+++ src/op.ts
@@ -3,8 +3,6 @@ import { App, DirectiveBinding } from "vue";
 export default (app: App) => {
   app.directive("op", {
     mounted(el: HTMLElement, binding: DirectiveBinding) {
-      console.log(el, binding);
-      console.log(hasOp(binding.value), 555);
       if (!hasOp(binding.value)) {
         el.style.display = "none";
       }
